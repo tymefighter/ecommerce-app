8b216ffb15ba9680955e676793576eef1a7c0b1a
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2024-02-21 20:36:10 +0530

    feat: Adds initial backend code

---
+++ ecommerce-app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.ecommerce"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-graphql")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework:spring-webflux")
+	testImplementation("org.springframework.graphql:spring-graphql-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
